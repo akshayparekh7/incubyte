d313e2a670fa2d362ed59c03aeb591e11374bf8b
Akshay <akshay07@Akshays-MacBook-Air.local>
2024-03-10 00:02:49 +0530

    Allow initial direction input in script

--- script.rb
+++ script.rb
@@ -1,19 +1,14 @@
 require 'json'
 
-def validate(input)
-  JSON.parse(input)
-rescue JSON::ParserError => e
-  puts 'Invalid input format. Please enter an array'
-  exit
-end
-
 puts "Input the Commands array, for eg ->  ['f', 'b', 'l', 'r'] "
-input = gets.chomp
-commands = validate(input)
+commands = gets.chomp
 
 puts 'Input the starting position array, for eg -> [0,0,0]'
 position_input = gets.chomp
-position = validate(position_input)
+
+puts 'Input the initial direction'
+direction = gets.chomp
 
 puts commands
-puts position
+puts position_input
+puts direction
