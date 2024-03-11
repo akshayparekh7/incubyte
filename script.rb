70f243724be80ca8aca815a60d7cac28a2ba9e5f
Akshay <akshay07@Akshays-MacBook-Air.local>
2024-03-11 11:39:03 +0530

    Integrate end to end

--- script.rb
+++ script.rb
@@ -1,14 +1,17 @@
 require 'json'
+require_relative 'src/spacecraft'
 
-puts "Input the Commands array, for eg ->  ['f', 'b', 'l', 'r'] "
+def convert_to_array(input)
+  eval(input)
+end
+
+puts "Input the Commands array, for eg ->  ['f', 'r', 'u', 'b', 'l'] "
 commands = gets.chomp
 
 puts 'Input the starting position array, for eg -> [0,0,0]'
 position_input = gets.chomp
 
-puts 'Input the initial direction'
+puts 'Input the initial direction, for eg N'
 direction = gets.chomp
 
-puts commands
-puts position_input
-puts direction
+Spacecraft.new(convert_to_array(position_input), direction, convert_to_array(commands)).call
