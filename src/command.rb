e3e3d17380286cc2843549cd7e1ef941f9b3a506
Akshay <akshay07@Akshays-MacBook-Air.local>
2024-03-11 11:38:22 +0530

    Maintain current direction in Command

--- src/command.rb
+++ src/command.rb
@@ -1,8 +1,9 @@
 # Base class for all the different types of command. 
 # receiver is the object of the class which will be responsible for the action
 class Command
-  def initialize(receiver)
+  def initialize(receiver, current_direction='')
     @receiver = receiver
+    @current_direction = current_direction
   end
 
   def execute
